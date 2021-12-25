764b7aaba163400d272cf49e350b23c7b4127475
Alex Nasser <alexandern83@gmail.com>
2021-12-25 17:24:06 +0000

    Coding Standard fix

--- fluid/SOURCE/MATH/math_square_root.h
+++ fluid/SOURCE/MATH/math_square_root.h
@@ -5,44 +5,7 @@
 
 class MATH_SQUARE_ROOT
 {
-	// -- PUBLIC
-
 public:
-
-	// .. CONSTRUCTORS
-
-	MATH_SQUARE_ROOT(
-		void
-		)
-	{
-	}
-
-	// ~~
-
-	MATH_SQUARE_ROOT(
-		MATH_SQUARE_ROOT & other
-		)
-	{
-	}
-
-	// ~~
-
-	virtual ~MATH_SQUARE_ROOT(
-		void
-		)
-	{
-	}
-
-	// .. OPERATORS
-
-	MATH_SQUARE_ROOT & operator=(
-		const MATH_SQUARE_ROOT & other
-		)
-	{
-		return *this;
-	}
-
-	// .. FUNCTIONS
 
 	// The Code and Algorithm used here is
 	// based on :
@@ -70,12 +33,12 @@ public:
 		} conv;
 
 		float x2;
-		const float threehalfs = 1.5F;
+		const float three_halfs = 1.5F;
 
 		x2 = value * 0.5F;
 		conv.f = value;
 		conv.i = 0x5f3759df - (conv.i >> 1);
-		conv.f = conv.f * (threehalfs - (x2 * conv.f * conv.f));
+		conv.f = conv.f * (three_halfs - (x2 * conv.f * conv.f));
 		return conv.f;
 	}
 };
